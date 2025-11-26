ce245a801e6778322f1edd5c471e26c83493b8a3
Demian Myers <murky.knight@gmail.com>
2025-11-26 21:07:12 +0100

    Refactor TypeScript configuration files and add test configuration to fix test deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,33 +8,35 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   test: {
-    environment: 'jsdom',
-    // 'jsdom' if you test browser APIs/React components
-    globals: true,
-    // enables describe/test/it/expect as globals
-    setupFiles: ['./vitest.setup.ts'] // enables testing libary globals
-    ,
+    environment: 'jsdom', // 'jsdom' if you test browser APIs/React components
+    globals: true, // enables describe/test/it/expect as globals
+    setupFiles: ['./vitest.setup.ts'], // enables testing libary globals
 
-    projects: [{
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: playwright({}),
-          instances: [{
-            browser: 'chromium'
-          }]
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, '.storybook'),
+          }),
+        ],
+        test: {
+          name: 'storybook',
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: playwright({}),
+            instances: [
+              {
+                browser: 'chromium',
+              },
+            ],
+          },
+          setupFiles: ['.storybook/vitest.setup.ts'],
         },
-        setupFiles: ['.storybook/vitest.setup.ts']
-      }
-    }]
-  }
-});
+      },
+    ],
+  },
+})
