8068943a26ed96f3694a5fb9aefb947b4f42d493
Demian Myers <murky.knight@gmail.com>
2025-12-28 11:14:59 +0100

    refactor: GameBoard to use updated House with storybook file

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,21 +1,17 @@
-import House from './House'
 import './GameBoard.css'
+import House from './House'
+
+export const HOUSES = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// TODO: need to lift colours used in multiple CSS files into a custom var (like border colour)
 
 export default function GameBoard() {
   return (
     <div className="container">
       <div className="game-board-container">
-        <House additionalClasses="border-right-thick-1 border-bottom-thick-1" number={1} />
-        <House additionalClasses="border-right-thick-1 border-bottom-thick-1" number={2} />
-        <House additionalClasses="border-bottom-thick-1" number={3} />
-
-        <House additionalClasses="border-right-thick-1 border-bottom-thick-1" number={4} />
-        <House additionalClasses="border-right-thick-1 border-bottom-thick-1" number={5} />
-        <House additionalClasses="border-bottom-thick-1" number={6} />
-
-        <House additionalClasses="border-right-thick-1" number={7} />
-        <House additionalClasses="border-right-thick-1" number={8} />
-        <House additionalClasses="" number={9} />
+        {HOUSES.map((house) => {
+          return <House key={`house-${house}`} number={house} />
+        })}
       </div>
     </div>
   )
