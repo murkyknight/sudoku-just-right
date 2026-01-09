9ae8809581b44c354ab2990252efd19ab95c10df
Demian Myers <murky.knight@gmail.com>
2026-01-09 21:47:08 +0100

    refactor: add comment to relax linting for non-null assertion in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import GameBoard from './components/GameBoard.tsx'
 import './styles/global.css'
 
+// biome-ignore lint/style/noNonNullAssertion: creating root, relax.
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
   <GameBoard />,
