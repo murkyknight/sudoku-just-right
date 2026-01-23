d3f97111dfc26ee2718103c467d97d2d7326ed8c
Demian Myers <murky.knight@gmail.com>
2026-01-23 16:25:14 +0100

    test: add storybook gameboard test for conflict feature

--- src/components/testLib/storybook/decorators.tsx
+++ src/components/testLib/storybook/decorators.tsx
@@ -2,7 +2,7 @@ import type { Decorator } from '@storybook/react-vite'
 import { useEffect } from 'react'
 import cellStyles from '../../Cell/Cell.module.css'
 import useGameStore, { type StoreState } from '../../store/useGameStore'
-import { createStoreState } from '../helpers'
+import { createBoard, createStoreState } from '../helpers'
 
 export const withGameStore: Decorator = (Story, { parameters }) => {
   const state: Partial<StoreState> = parameters.state ?? {}
@@ -12,6 +12,22 @@ export const withGameStore: Decorator = (Story, { parameters }) => {
 
   return <Story />
 }
+
+export const withFullBoardGameStore: Decorator = (Story, { parameters }) => {
+  const state: Partial<StoreState> = parameters.state ?? {}
+  useEffect(() => {
+    useGameStore.setState(
+      createStoreState({
+        board: createBoard(),
+        ...state,
+      }),
+    )
+  }, [state])
+
+  return <Story />
+}
+
+// TODO: new function withFullBoardGameStore
 
 export const withOutsideDiv: Decorator = (Story, _context) => {
   return (
