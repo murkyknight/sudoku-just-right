44fd0df2b959060fcce917400c150519f7380585
Demian Myers <murky.knight@gmail.com>
2026-01-27 21:17:07 +0100

    fix: exclude storybook from build
    - Now we can build our app

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,3 +4,8 @@ declare module '*.module.css' {
   const classes: Record<string, string>
   export default classes
 }
+
+declare module '*.css' {
+  const content: { [className: string]: string }
+  export default content
+}
