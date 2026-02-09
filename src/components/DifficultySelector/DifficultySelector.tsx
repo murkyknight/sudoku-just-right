436d2bc27b7e8ea327ed135a7a9a3832f5e2965a
Demian Myers <murky.knight@gmail.com>
2026-02-09 20:12:42 +0100

    feat: add useDifficulty to DifficultySelector so we can loading a difficulty - Note: puzzle is not loaded in Zustand yet

--- src/components/DifficultySelector/DifficultySelector.tsx
+++ src/components/DifficultySelector/DifficultySelector.tsx
@@ -1,10 +1,34 @@
+import { difficultyType, type Difficulty } from '@/types'
+import { useState } from 'react'
 import { ToggleGroup, ToggleGroupItem } from '../ui/toggle-group'
 import styles from './DifficultySelector.module.css'
+import useDifficulty from './hooks/useDifficulty'
 
 export default function DifficultySelector() {
+  const [difficulty, setDifficulty] = useState<Difficulty>(difficultyType.EASY)
+  const { puzzles, loadNextSudoku, currentSudoku, isLoading } = useDifficulty({ difficulty })
+  console.log('useDifficulty - currentSudoku: ', { puzzles, currentSudoku, isLoading })
+
+  const handleChange = (newDifficulty: string) => {
+    if (!newDifficulty) {
+      // if same difficulty selected, empty string is passed
+      return loadNextSudoku()
+    }
+
+    if (newDifficulty.toUpperCase() in difficultyType) {
+      setDifficulty(newDifficulty as Difficulty)
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <ToggleGroup defaultValue="easy" type="single" variant="outline">
+      <ToggleGroup
+        defaultValue="easy"
+        onValueChange={handleChange}
+        type="single"
+        value={difficulty}
+        variant="outline"
+      >
         <ToggleGroupItem aria-label="Toggle Breezy" value="easy">
           Breezy
         </ToggleGroupItem>
